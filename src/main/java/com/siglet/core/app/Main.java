f713345d279f61a3bea9138cfd885659d7796eb3
Dilver-Corales <dilvercorales23@gmail.com>
2026-07-24 00:21:37 -0500

    test: verificar el funcionamiento de los estados

--- src/main/java/com/siglet/core/app/Main.java
+++ src/main/java/com/siglet/core/app/Main.java
@@ -1,7 +1,20 @@
 package com.siglet.core.app;
 
+import com.siglet.core.persistence.domain.Operario;
+import com.siglet.core.persistence.enumeration.EstadoOperario;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("SIGLET ejecutandose");
+
+        Operario operario = new Operario("73493012","Martin","Gaspar");
+
+        System.out.println("OPERARIO EJECUTADO su estado es: " + operario.estaDisponible());
+
+        operario.cambiarEstado(EstadoOperario.TRABAJANDO);
+
+        System.out.println("OPERARIO EJECUTADO su estado cambio es es: " + operario.estaDisponible());
+
+
     }
 }
