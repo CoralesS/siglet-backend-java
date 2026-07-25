183fccc96a3870cf9568d22e70724419051e50d5
Dilver-Corales <dilvercorales23@gmail.com>
2026-07-24 22:46:56 -0500

    feat: agregar getters

--- src/main/java/com/siglet/core/persistence/domain/Ticket.java
+++ src/main/java/com/siglet/core/persistence/domain/Ticket.java
@@ -3,20 +3,45 @@ package com.siglet.core.persistence.domain;
 import java.util.List;
 
 public class Ticket {
-    private String codigo;
+    private int codigo;
+    private Lote lote;
+    private Operario operario;
+    private  MaquinaEtiquetado maquinaEtiquetado;
 
     // constructor
-    public Ticket() {
-        this.codigo = "A-";
+    public Ticket(Lote lote, Operario operario,  MaquinaEtiquetado maquinaEtiquetado) {
+        this.lote = lote;
+        this.operario = operario;
+        this.maquinaEtiquetado = maquinaEtiquetado;
     }
 
     // metodos
-    public List<Ticket> buscarPorLoteCodigo(String codigolote){
-        return null;
-    };
+    public String getCodigo() {
+        return "A-" + String.format("%05d", this.codigo);
+    }
 
-    public List<Ticket> buscarTodo(){
-        return null;
+    public Lote getLote() {
+        return lote;
+    }
+
+    public void setLote(Lote lote) {
+        this.lote = lote;
+    }
+
+    public Operario getOperario() {
+        return operario;
+    }
+
+    public void setOperario(Operario operario) {
+        this.operario = operario;
+    }
+
+    public MaquinaEtiquetado getMaquinaEtiquetado() {
+        return maquinaEtiquetado;
+    }
+
+    public void setMaquinaEtiquetado(MaquinaEtiquetado maquinaEtiquetado) {
+        this.maquinaEtiquetado = maquinaEtiquetado;
     }
 }
 
