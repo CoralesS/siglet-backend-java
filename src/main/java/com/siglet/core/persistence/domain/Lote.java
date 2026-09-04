36652e4c264ef8148c050e7cdbd16c1c48f016df
Dilver-Corales <dilvercorales23@gmail.com>
2026-09-04 13:10:32 -0500

    feat: completar getters y setters

--- src/main/java/com/siglet/core/persistence/domain/Lote.java
+++ src/main/java/com/siglet/core/persistence/domain/Lote.java
@@ -16,10 +16,11 @@ public class Lote {
     private MaquinaEtiquetado maquinaEtiquetado;
 
     // constructor
-    public Lote(int codigo, int cantidad){
-        this.codigo = codigo;
+    public Lote(int cantidad){
         this.cantidad = cantidad;
+        this.prioridadLote = PrioridadLote.NORMAL;
     }
+    public Lote(){}
 
     // Metodos
     public void iniciarProceso() {
@@ -53,6 +54,50 @@ public class Lote {
 
     public int getCodigo() {
         return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public LocalDateTime getInicioProceso() {
+        return inicioProceso;
+    }
+
+    public LocalDateTime getFinProceso() {
+        return finProceso;
+    }
+
+    public PrioridadLote getPrioridadLote() {
+        return prioridadLote;
+    }
+
+    public MaquinaEtiquetado getMaquinaEtiquetado() {
+        return maquinaEtiquetado;
+    }
+
+    public void setInicioProceso(LocalDateTime inicioProceso) {
+        this.inicioProceso = inicioProceso;
+    }
+
+    public void setFinProceso(LocalDateTime finProceso) {
+        this.finProceso = finProceso;
+    }
+
+    public void setPrioridadLote(PrioridadLote prioridadLote) {
+        this.prioridadLote = prioridadLote;
+    }
+
+    public void setMaquinaEtiquetado(MaquinaEtiquetado maquinaEtiquetado) {
+        this.maquinaEtiquetado = maquinaEtiquetado;
     }
 }
 
