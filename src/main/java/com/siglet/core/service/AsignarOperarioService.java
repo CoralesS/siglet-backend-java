0bae7ce279dba049b598ed38a5e9be9995ea1410
Dilver-Corales <dilvercorales23@gmail.com>
2026-09-04 13:12:11 -0500

    feat: agregar logica de busqueda y guardado

--- src/main/java/com/siglet/core/service/AsignarOperarioService.java
+++ src/main/java/com/siglet/core/service/AsignarOperarioService.java
@@ -1,7 +1,24 @@
 package com.siglet.core.service;
 
+import com.siglet.core.persistence.domain.MaquinaEtiquetado;
+import com.siglet.core.persistence.repository.MaquinaEtiquetadoRepository;
+import com.siglet.core.persistence.repository.OperarioRepository;
+
 public class AsignarOperarioService {
+
+    MaquinaEtiquetadoRepository mEtiqRepository;
+    OperarioRepository oRepository;
+
+    public AsignarOperarioService(MaquinaEtiquetadoRepository mEtiqRepository, OperarioRepository oRepository) {
+        this.mEtiqRepository = mEtiqRepository;
+        this.oRepository = oRepository;
+    }
+
     public void ejecutarAsignarOperario(String idMaquina, String dNI) {
+
+        // Consultar repositorio para obtener la entidad
+
+
 
     }
 }
