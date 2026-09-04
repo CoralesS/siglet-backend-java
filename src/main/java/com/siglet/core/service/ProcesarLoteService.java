7c05b0995be4e773a57afe303b8b4305339b7d3f
Dilver-Corales <dilvercorales23@gmail.com>
2026-09-04 16:44:51 -0500

    feat: agregar logica para procesar lotes

--- src/main/java/com/siglet/core/service/ProcesarLoteService.java
+++ src/main/java/com/siglet/core/service/ProcesarLoteService.java
@@ -1,12 +1,50 @@
 package com.siglet.core.service;
 
+import com.siglet.core.persistence.domain.Lote;
+import com.siglet.core.persistence.domain.MaquinaEtiquetado;
+import com.siglet.core.persistence.domain.Operario;
+import com.siglet.core.persistence.domain.Ticket;
+import com.siglet.core.persistence.enumeration.EstadoMaquina;
+import com.siglet.core.persistence.repository.LoteRepository;
+import com.siglet.core.persistence.repository.MaquinaEtiquetadoRepository;
+import com.siglet.core.persistence.repository.TicketRepository;
+
 public class ProcesarLoteService {
 
-    public void iniciarProcesoLote(String codigoLote){
+    MaquinaEtiquetadoRepository mEtiqRepository;
+    LoteRepository loteRepository;
+    TicketRepository ticketRepository;
 
+    public ProcesarLoteService(MaquinaEtiquetadoRepository mEtiqRepository, LoteRepository loteRepository) {
+        this.mEtiqRepository = mEtiqRepository;
+        this.loteRepository = loteRepository;
     }
 
-    public void finalizarProcesoLote(String codigoLote){
+    public void iniciarProcesoLote(int codigoLote){
+        Lote lote = loteRepository.buscarPorCodigo(codigoLote);
+
+        if(lote == null) {
+            throw new RuntimeException("Lote no encontrado " + codigoLote);
+        }
+
+        lote.iniciarProceso();
+        loteRepository.guardar(lote);
+    }
+
+    public void finalizarProcesoLote(int codigoLote){
+        Lote lote = loteRepository.buscarPorCodigo(codigoLote);
+        if(lote == null) {
+            throw new RuntimeException("Lote no encontrado " + codigoLote);
+        }
+
+        lote.finalizarProceso();
+
+        MaquinaEtiquetado maquinaUsada = lote.getMaquinaEtiquetado();
+        Operario operarioInvolucrado = maquinaUsada.getOperario();
+
+        Ticket nuevoTicket = new Ticket(lote, operarioInvolucrado, maquinaUsada);
+        loteRepository.guardar(lote);
+        ticketRepository.guardar(nuevoTicket);
 
     }
 }
