55ad48af63518ecf3233d81db358739caa2cad6a
Dilver-Corales <dilvercorales23@gmail.com>
2026-09-04 16:01:39 -0500

    feat: agregar logica de negocio para asignar operario a maquina

--- src/main/java/com/siglet/core/service/AsignarOperarioService.java
+++ src/main/java/com/siglet/core/service/AsignarOperarioService.java
@@ -1,6 +1,9 @@
 package com.siglet.core.service;
 
 import com.siglet.core.persistence.domain.MaquinaEtiquetado;
+import com.siglet.core.persistence.domain.Operario;
+import com.siglet.core.persistence.enumeration.EstadoMaquina;
+import com.siglet.core.persistence.enumeration.EstadoOperario;
 import com.siglet.core.persistence.repository.MaquinaEtiquetadoRepository;
 import com.siglet.core.persistence.repository.OperarioRepository;
 
@@ -17,8 +20,28 @@ public class AsignarOperarioService {
     public void ejecutarAsignarOperario(String idMaquina, String dNI) {
 
         // Consultar repositorio para obtener la entidad
+        MaquinaEtiquetado maquina = mEtiqRepository.buscarPorId(idMaquina);
+        Operario operario = oRepository.buscarPorDNI(dNI);
 
+        // Validar si las entidades existen
+        if (maquina == null || operario == null) {
+            throw new RuntimeException("Operario no encontrado");
+        }
 
+        // Validar si la maquina esta libre y operario disponible
+        if (!maquina.estaDisponible()) {
+            throw new  RuntimeException("Maquina no encontrada");
+        }
 
+        if (!operario.estaDisponible()) {
+            throw new  RuntimeException("Operario no encontrado");
+        }
+
+        // Cambio de estado de la maquina y operario
+        maquina.setOperario(operario);
+        operario.setEstadoOperario(EstadoOperario.TRABAJANDO);
+
+        // guardar cambios en la BD
+        mEtiqRepository.guardar(maquina);
     }
 }
