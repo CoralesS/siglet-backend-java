57f8f8cc261a6bfbaf7576471e8a3ed6a9dd4f02
Dilver-Corales <dilvercorales23@gmail.com>
2026-09-04 22:08:16 -0500

    feat: logica para levantar el servidor

--- src/main/java/com/siglet/core/app/Main.java
+++ src/main/java/com/siglet/core/app/Main.java
@@ -1,20 +1,41 @@
 package com.siglet.core.app;
 
-import com.siglet.core.persistence.domain.Operario;
-import com.siglet.core.persistence.enumeration.EstadoOperario;
+import com.siglet.core.persistence.repository.LoteRepository;
+import com.siglet.core.persistence.repository.MaquinaEtiquetadoRepository;
+import com.siglet.core.persistence.repository.OperarioRepository;
+import com.siglet.core.persistence.repository.TicketRepository;
+import com.siglet.core.presentation.controller.AsignarOperarioController;
+import com.siglet.core.presentation.controller.ProcesarLoteController;
+import com.siglet.core.service.AsignarOperarioService;
+import com.siglet.core.service.ProcesarLoteService;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("SIGLET ejecutandose");
+    public static void main(String[] args) throws IOException {
+        // 1. Instanciar todos los repositorios
+        MaquinaEtiquetadoRepository maquinaRepo = new MaquinaEtiquetadoRepository();
+        LoteRepository loteRepo = new LoteRepository();
+        TicketRepository ticketRepo = new TicketRepository();
+        OperarioRepository operarioRepo = new OperarioRepository();
 
-        Operario operario = new Operario("73493012","Martin","Gaspar");
+        // 2. Instanciar los servicios inyectando sus repositorios
+        ProcesarLoteService loteService = new ProcesarLoteService(maquinaRepo, loteRepo, ticketRepo);
+        AsignarOperarioService operarioService = new AsignarOperarioService(operarioRepo);
 
-        System.out.println("OPERARIO EJECUTADO su estado es: " + operario.estaDisponible());
+        // 3. Crear el servidor HTTP
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        operario.cambiarEstado(EstadoOperario.TRABAJANDO);
+        // 4. Conectar las rutas (URLs) con su respectivo controlador
+        server.createContext("/api/lote", new ProcesarLoteController(loteService));
+        server.createContext("/api/operario", new AsignarOperarioController(operarioService));
 
-        System.out.println("OPERARIO EJECUTADO su estado cambio es es: " + operario.estaDisponible());
+        server.setExecutor(null);
+        server.start();
 
-
+        System.out.println("Servidor web iniciado en http://localhost:8080");
     }
+
 }
